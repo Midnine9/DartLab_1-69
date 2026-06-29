6df30e0c9a8833e1b6111a8a537d2735eb5312af
unknown <monkong147@gmail.com>
2026-06-29 12:16:34 +0700

    เสร็จแล้วจ้า

--- lab2_exercise3.dart
+++ lab2_exercise3.dart
@@ -1,14 +1,37 @@
 void main(){
+  print("ข้อ3");
   List <String> subjects = ['Math','Physic','Chem'];
   print(subjects.length);
+
+  print("ข้อ4");
   print(subjects[0] +" " + subjects[2]);
+
+  print("ข้อ5");
   subjects.add('PE');
   print(subjects);
+
+  print("ข้อ6");
   Map <String,int> studentScores = {
     'Math': 85,
     'Physic' : 50
   };
+  print("ข้อ7");
   print(studentScores['Math']);
+
+  print("ข้อ8");
   studentScores['PE'] = 95;
   print(studentScores);
+
+  print("ข้อ9");
+  for (var i in studentScores.entries){
+    if (i.key.contains("a")){
+      print('${i.key} : ${i.value}');
+    }
+  }
+  print("ข้อ10");
+  for (var y in studentScores.entries){
+    if (y.value >= 50){
+      print('${y.key} : ${y.value}');
+    }
+  }
 }

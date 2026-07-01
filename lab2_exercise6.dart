1041ef8666c9432b11871e1c89790f23181f1ffe
Midnine9 <monkong147@gmail.com>
2026-07-01 15:06:14 +0700

    uy

--- lab2_exercise6.dart
+++ lab2_exercise6.dart
@@ -1,9 +1,9 @@
 import 'dart:ffi';
 
-void executeOperation(int w, int h, int Function(int, int) operation){
+void executeOperation(double w, double h, double Function(double, double) operation){
   var result = operation(w , h);
   print('Result : $result');
 }
-Float square (Float x , Float y){
+double square (double x , double y){
   return (x*y) ;
 }
